0af2cd6c8a1d22e0692367e048bbe8a1001dd55d
a18980620 <Masters5>
2021-10-02 11:40:05 +0300

    fix some things

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 def index():
     with open('resources/swagger_example.json', 'r', encoding='utf-8') as f:
         filetext = f.read()
+# todo обработка исключений чтения файла, затем вынести в функцию
 
     return render_template('index.html', filetext=filetext)
 
