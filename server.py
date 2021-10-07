10948831036253042ae1b2b061864463b97ac38a
a18980620 <Masters5>
2021-10-07 22:58:22 +0300

    send data to server

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,10 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     with open('resources/swagger_example.json', 'r', encoding='utf-8') as f:
+        # todo обработка исключений чтения файла, затем вынести в функцию
         filetext = f.read()
-# todo обработка исключений чтения файла, затем вынести в функцию
-
     return render_template('index.html', filetext=filetext)
+
+
+@app.route('/swagger', methods=['POST'])
+def receive_swagger():
+    # todo добавить обработку исключений
+    data = request.json
+    print("HELLO ", data)
+    return {}
 
 
 if __name__ == '__main__':
