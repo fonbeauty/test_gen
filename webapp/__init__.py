63161cb7713410cfa0f465467ead842318eefb7d
a18980620 <Masters5>
2021-11-07 22:53:29 +0300

    add login user

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_login import LoginManager, login_user, logout_user
+
 from datetime import datetime
+
 from webapp.tests import test_execute
 from webapp.send_requests import standart_tests
 from webapp.forms import LoginForm
-
-
-from webapp.model import db, Swagger
+from webapp.model import db, Swagger, User
 
 
 def create_app():
@@ -13,11 +14,42 @@ def create_app():
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
+
     @app.route('/login')
     def login():
         title = 'Авторизация'
         login_form = LoginForm()
         return render_template('login.html', page_title=title, form=login_form)
+
+
+    @app.route('/logout')
+    def logout():
+        logout_user()
+        flash('Вы успешно разлогинились')
+        return redirect(url_for('index'))
+
+
+    @app.route('/process-login', methods=['POST'])
+    def process_login():
+        form = LoginForm()
+
+        if form.validate_on_submit():
+            user = User.query.filter(User.username == form.username.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user)
+                flash('Вы успешно вошли на сайт')
+                return redirect(url_for('index'))
+
+        flash('Неправильные имя или пароль пользователя')
+        return redirect(url_for('login'))
+
 
 
     @app.route('/')
--- webapp/model.py
+++ webapp/model.py
@@ -1,4 +1,6 @@
+from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -12,4 +14,20 @@ class Swagger(db.Model):
     swagger = db.Column(db.JSON, nullable=False)
 
     def __repr__(self):
-        return '<Swagger {} {}>',format(self.title, self.author)
+        return '<Swagger {} {}>', format(self.title, self.author)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), index=True, unique=True)
+    password = db.Column(db.String(128))
+    role = db.Column(db.String(10), index=True)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
